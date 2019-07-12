f969b124abaab10372e0a43149f9e33bbc38b34a
brandotheturner <brandontheturner@gmail.com>
2019-07-11 23:40:24 -0500

    comments

--- app.js
+++ app.js
@@ -17,10 +17,14 @@ $(document).ready(function() {
 
   const services = returnValues.map(createService);
 
+  // created race function to loop through services and modify DOM elements
   const race = array => {
     let counter = 1;
+    // forEach to loop through array of services (takes current function and index parameters)
     array.forEach((func, i) => {
+      // calls function at index within services array and supplies parameters retVal and index
       func(returnValues[i], i).then(result => {
+        // after promise resolves modify text
         $("#service" + i).text(returnValues[i]);
         $("#service" + i + "Status").text("DONE");
         $("#service" + i + "Place").text(counter + ". ");
@@ -28,6 +32,6 @@ $(document).ready(function() {
       });
     });
   };
-
+  //starts race
   race(services);
 });
